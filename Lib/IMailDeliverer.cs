4e1ad9bfee1fed86d6af7684e88706bf970559fe
Drew Miller <half-ogre@users.noreply.github.com>
2020-01-12 19:21:28 -0800

    move to dotnet core

--- Lib/IMailDeliverer.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Net.Mail;
-
-namespace AnglicanGeek.MarkdownMailer
-{
-    public interface IMailDeliverer
-    {
-        public void Deliver(MailMessage mailMessage);
-    }
-}
--- Lib/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("MarkdownMailer")]
-[assembly: AssemblyProduct("MarkdownMailer")]
-[assembly: AssemblyCompany("Andrew Miller <nemo@half-ogre.com>")]
-[assembly: AssemblyCopyright("Copyright (C) 2011 Andrew Miller <nemo@half-ogre.com>")]
-[assembly: AssemblyVersion("1.2.1.0")]
-[assembly: ComVisible(false)]
